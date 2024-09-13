6ada6fbe5a211b1751b295808159dca0a8dfee1b
ShanglinWu <jasonwu1017@gmail.com>
2024-09-13 17:27:06 -0500

    init dataset code

---
+++ dataset/generate.sh
@@ -0,0 +1,2 @@
+#python run_experiment.py --model-name json --model-size text-ada-001 --distractors none --test-distractors none --max-hops 1 # simple no distractors
+python run_experiment.py --model-name json --model-size text-ada-001 --max-hops 1 --generate-trio #with distractors
